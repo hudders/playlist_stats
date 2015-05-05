7363c780cf3e8bfd50cad15b4a6daa14233ba28c
Michael Hudson <hudders@gmail.com>
2015-05-05 11:17:47 +0100

    MWH - changed interaction so you have to @makeshift to get replies

--- makeshift.rb
+++ makeshift.rb
@@ -9,23 +9,29 @@ fail auth['error'] unless auth['ok']
 
 client = Slack.realtime
 
+def reply(data, text)
+	Slack.chat_postMessage channel: data['channel'], username: "makeshift", icon_url: "http://api.doodlecircus.co.uk/makeshift.jpg", text: text
+end
+
 client.on :hello do
-	# logger.info 'Successfully connected.'
 	puts "connected"
 end
 
 client.on :message do |data|
 	case data['text']
-		when 'makeshiftbot test'
-			if data['user'] = "U02D7MQFW"
-				Slack.chat_postMessage channel: data['channel'], text: "OK boss"
-			end
-		when 'makeshiftbot playlist stats'
+		when '<@U04MZH46B>: stats'
 			load 'get_stats.rb'
-			Slack.chat_postMessage channel: data['channel'], text: "#{$counted}"
-		when 'makeshiftbot playlist total'
+			reply(data, "#{$counted}")
+		when '<@U04MZH46B>: total'
 			load 'get_stats.rb'
-			Slack.chat_postMessage channel: data['channel'], text: "There are #{$number_of_tracks} tracks in the #{$playlist_name} playlist."
+			reply(data, "There are #{$number_of_tracks} tracks in the #{$playlist_name} playlist.")
+		end
+	case data['user']
+		when "U02D7MQFW"
+			case data['text']
+				when "<@U04MZH46B>: test"
+					reply(data, "Everything's A-OK boss. :smile:")
+				end
 		end
 end
 
